c10960cddd095c3eb846297b963f0dbb8ba13849
vladbuk <vladbuk@gmail.com>
2018-06-07 07:10:09 +0300

    added environment and init files

---
+++ bootstrap/init.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * Start session if not alreasy started
+ */
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+/**
+ * Load environment variables
+ */
+require_once __DIR__ . '/../app/config/_env.php';
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // echo "Index.php";
 
-require_once __DIR__ . '/../app/config/_env.php';
+require_once __DIR__ . '/../bootstrap/init.php';
 
 $app_name = getenv('APP_NAME');
 
